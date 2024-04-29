39776da9e6912624b9472483b04e4cac2e2ca475
Isaiah Cornelius <112984277+Isaiah-Cornelius@users.noreply.github.com>
2024-04-29 22:52:50 +0000

    Updated Certified NCDFIs map

--- Certified-NCDFIs/config.js
+++ Certified-NCDFIs/config.js
@@ -5,12 +5,12 @@ const config = {
   style: 'mapbox://styles/isaiahcornelius/clqg37fke005y01pz7d7j95wr',
   accessToken:
     'pk.eyJ1IjoiaXNhaWFoY29ybmVsaXVzIiwiYSI6ImNsbzJldHVwdzFmMWEya3FoNGo5ZXoybG8ifQ.P4y494VuvnDyeTQDNtEoDA',
-  CSV: './2023CertifiedNCDFIs.csv',
+  CSV: './4.15.2024_CertifiedNCDFIs.csv',
   center: [-117, 48],
   zoom: 1,
   title: 'Certified Native CDFIs',
   description:
-    '2023 Certified Native Community Development Financial Institutions of the United States of America.',
+    'Certified Native Community Development Financial Institutions of the United States of America. Updated 04/15/2024',
   sideBarInfo: ['Location_Name', 'Address', 'City_ST_Zip', 'Phone'],
   popupLocation_Name: ['Location_Name'],
   popupWebsite: ['Website'],

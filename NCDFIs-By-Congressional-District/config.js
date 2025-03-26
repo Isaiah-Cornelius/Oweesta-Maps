bd0003a543a2c846e7653048e0b68b877d0416c1
Isaiah Cornelius <112984277+Isaiah-Cornelius@users.noreply.github.com>
2025-03-26 00:07:19 +0000

    updated style

--- NCDFIs-By-Congressional-District/config.js
+++ NCDFIs-By-Congressional-District/config.js
@@ -2,7 +2,7 @@
 
 // eslint-disable-next-line no-unused-vars
 const config = {
-  style: 'mapbox://styles/isaiahcornelius/cm8gdvt3b012m01ssagcje82a',
+  style: 'mapbox://styles/isaiahcornelius/cm8p5cwlv007k01sn56kwan7k',
   accessToken:
     'pk.eyJ1IjoiaXNhaWFoY29ybmVsaXVzIiwiYSI6ImNsbzJldHVwdzFmMWEya3FoNGo5ZXoybG8ifQ.P4y494VuvnDyeTQDNtEoDA',
   CSV: './3.12.2025_CertifiedNCDFIs.csv',
